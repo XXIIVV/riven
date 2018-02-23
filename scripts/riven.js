2113d1d1df9e68803a87764458157257d95e5b03
Devine Lu Linvega <aliceffekt@gmail.com>
2018-02-24 11:35:49 +1300

    Let's do this

--- scripts/riven.js
+++ scripts/riven.js
@@ -1,25 +1,43 @@
-function Node(id)
-{
-  this.id = id
-  this.ports = {in:null,out:null};
-
-  this.bang = function()
-  {
-    console.log(`Bang! ${this.id}`)
-    if(this.ports.out){
-      this.ports.out.bang();  
-    }
-  }
-
-  this.connect = function(target)
-  {
-    this.ports.out = target
-  }
-}
-
 function Ø()
 {
-  this.connect = function(){
+  this.network = {};
+
+  this.create = function(name)
+  {
+    this.network[name] = new Node(name)
+  }
+
+  this.connect = function(a,b)
+  {
+    this.network[a].connect(this.network[b])
+  }
+
+  this.bang = function(a)
+  {
+    this.network[a].bang();
+  }
+
+  this.graph = function()
+  {
     return "hello"
+  }
+
+  function Node(id)
+  {
+    this.id = id;
+    this.ports = {in:null,out:null};
+
+    this.connect = function(b)
+    {
+      this.ports.out = b
+    }
+
+    this.bang = function()
+    {
+      console.log("bang",this.id)
+      if(this.ports.out){
+        this.ports.out.bang();  
+      }
+    }
   }
 }
