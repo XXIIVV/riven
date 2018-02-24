20e1e69479705102310288772301135a3f5e28cb
Devine Lu Linvega <aliceffekt@gmail.com>
2018-02-24 20:00:59 +1300

    Sorted out graphing and network code

--- scripts/riven.graph.js
+++ scripts/riven.graph.js
@@ -1,36 +1,47 @@
-Ø.prototype.graph = function()
+function Ø()
 {
-  var html = "";
+  Riven.call(this);
 
-  for(id in this.network){
-    var node = this.network[id];
-    html += node.graph();
-  }
-  this.el.innerHTML = html;
+  this.el = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+  this.el.style.width = "100vw"
+  this.el.style.height = "100vh"
 
-  Node.prototype.graph = function()
+  this.graph = function()
   {
-    var x = this.rect.x*grid_size;
-    var y = this.rect.y*grid_size;
-    var w = this.rect.w*grid_size;
-    var h = this.rect.h*grid_size;
+    var html = "";
+
+    for(id in this.network){
+      var node = this.network[id];
+      html += draw(node);
+    }
+    this.el.innerHTML = html;
+  }
+
+  function draw(node)
+  {
+    var grid_size = 20;
+
+    var x = node.rect.x*grid_size;
+    var y = node.rect.y*grid_size;
+    var w = node.rect.w*grid_size;
+    var h = node.rect.h*grid_size;
 
     var ports_html = "";
-    for(id in this.ports.in){
-      var port = this.ports.in[id];
-      var spacing = h/(this.ports.in.length)
+    for(id in node.ports.in){
+      var port = node.ports.in[id];
+      var spacing = h/(node.ports.in.length)
       ports_html += `<circle cx='${x}' cy="${y+(id*spacing)+(spacing/2)}" r="4" fill="black"/>`
     }
-    for(id in this.ports.out){
-      var port = this.ports.out[id];
-      var spacing = h/(this.ports.out.length)
+    for(id in node.ports.out){
+      var port = node.ports.out[id];
+      var spacing = h/(node.ports.out.length)
       ports_html += `<circle cx='${x+w}' cy="${y+(id*spacing)+(spacing/2)}" r="4" fill="black"/>`
     }
 
     return `
-    <text x="${x}" y="${y+h+(grid_size)}" font-size='11' font-family='input_mono_regular' stroke-width='0' fill='#000'>${this.id}</text>
+    <text x="${x}" y="${y+h+(grid_size)}" font-size='11' font-family='input_mono_regular' stroke-width='0' fill='#000'>${node.id}</text>
     <circle cx='${x}' cy="${y}" r="2" fill="black"/>
-    <rect x=${x} y=${y} width="${w}" height="${h}" title="alt" stroke="#000" fill="none" stroke-width="1.5"></rect>
+    <rect x=${x} y=${y} width="${w}" height="${h}" title="alt" stroke="#000" fill="none" stroke-width="1.5"/>
     ${ports_html}`
   }
 }
--- scripts/riven.js
+++ scripts/riven.js
@@ -1,14 +1,11 @@
-function Ø()
+function Riven()
 {
   var grid_size = 20;
-  this.el = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-  this.el.style.width = "100vw"
-  this.el.style.height = "100vh"
   this.network = {root:new Node(null,"root",{x:0,y:0,w:30,h:15})};
 
   this.create = function(parent,name,rect,ports)
   {
-    this.network[name] = new Node(parent,name,rect,ports)
+    this.network[name] = new Node(this.network[parent],name,rect,ports)
   }
 
   this.connect = function(a,b)
@@ -19,17 +16,6 @@ function Ø()
   this.bang = function(a)
   {
     this.network[a].bang();
-  }
-
-  this.graph = function()
-  {
-    var html = "";
-
-    for(id in this.network){
-      var node = this.network[id];
-      html += node.graph();
-    }
-    this.el.innerHTML = html;
   }
 
   function Node(parent,id,rect={x:0,y:0,w:5,h:5},ports={in:[],out:[]})
@@ -57,32 +43,6 @@ function Ø()
       // if(this.ports.out){
       //   this.ports.out.bang();  
       // }
-    }
-
-    this.graph = function()
-    {
-      var x = this.rect.x*grid_size;
-      var y = this.rect.y*grid_size;
-      var w = this.rect.w*grid_size;
-      var h = this.rect.h*grid_size;
-
-      var ports_html = "";
-      for(id in this.ports.in){
-        var port = this.ports.in[id];
-        var spacing = h/(this.ports.in.length)
-        ports_html += `<circle cx='${x}' cy="${y+(id*spacing)+(spacing/2)}" r="4" fill="black"/>`
-      }
-      for(id in this.ports.out){
-        var port = this.ports.out[id];
-        var spacing = h/(this.ports.out.length)
-        ports_html += `<circle cx='${x+w}' cy="${y+(id*spacing)+(spacing/2)}" r="4" fill="black"/>`
-      }
-
-      return `
-      <text x="${x}" y="${y+h+(grid_size)}" font-size='11' font-family='input_mono_regular' stroke-width='0' fill='#000'>${this.id}</text>
-      <circle cx='${x}' cy="${y}" r="2" fill="black"/>
-      <rect x=${x} y=${y} width="${w}" height="${h}" title="alt" stroke="#000" fill="none" stroke-width="1.5"></rect>
-      ${ports_html}`
     }
 
     function Port(host,id)
