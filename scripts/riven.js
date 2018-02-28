146c1acb26269565e264dc48f667c697b2c9171c
Devine Lu Linvega <aliceffekt@gmail.com>
2018-02-28 17:10:10 +1300

    Removed unused testing methods

--- scripts/riven.js
+++ scripts/riven.js
@@ -20,7 +20,7 @@ function Ø(s,network = RIVEN.network)
   if(s.indexOf(" ") > -1){
     var node_id = s.split(" ")[0];
     var port_id = s.split(" ")[1];
-    return network[node_id] && network[node_id].port(port_id) ? network[node_id].port(port_id) : null;
+    return network[node_id] && network[node_id].ports[port_id] ? network[node_id].ports[port_id] : null;
   }
   else if(network[s]){
     return network[s];
@@ -32,25 +32,21 @@ function Ø(s,network = RIVEN.network)
 
 // NODE
 
-function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
+function Node(id,rect={x:0,y:0,w:2,h:2})
 {
-  this.glyph = "M150,60 L150,60 L60,150 L150,240 L240,150 Z"
+  this.glyph = NODE_GLYPHS.default
   this.id = id;
-  this.ports = ports
+  this.ports = {}
   this.rect = rect;
-  this.rect.w = this.rect.w ? this.rect.w : 0
-  this.rect.h = this.rect.h ? this.rect.h : 0
   this.parent = null;
   this.children = [];
 
   this.setup = function()
   {
-    this.ports.push(new Port(this,"out",PORT_TYPES.output))
-    this.ports.push(new Port(this,"in",PORT_TYPES.input))
-    this.ports.push(new Port(this,"answer",PORT_TYPES.answer))
-    this.ports.push(new Port(this,"request",PORT_TYPES.request))
-    this.rect.w = this.rect.w ? this.rect.w : 0
-    this.rect.h = this.rect.h ? this.rect.h : 0
+    this.ports.input = new Port(this,"in",PORT_TYPES.input)
+    this.ports.output = new Port(this,"out",PORT_TYPES.output)
+    this.ports.answer = new Port(this,"answer",PORT_TYPES.answer)
+    this.ports.request = new Port(this,"request",PORT_TYPES.request)
   }
 
   this.create = function(pos)
@@ -100,7 +96,9 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
       }
     }
     else{
-      this.port(type == ROUTE_TYPES.request ? "request" : "out").connect(`${q} ${type == ROUTE_TYPES.request ? "answer" : "in"}`,type);  
+      // var port = this.ports[type == ROUTE_TYPES.request ? "request" : "out"];
+      console.log(this.ports)
+      this.ports[type == ROUTE_TYPES.request ? "request" : "output"].connect(`${q} ${type == ROUTE_TYPES.request ? "answer" : "input"}`,type);  
     }
   }
 
@@ -125,19 +123,16 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
 
   this.bang = function()
   {
-    console.log(`${this.id} bang!`)
     this.send(true)
   }
 
   this.send = function(payload)
   {
-    console.log(`${this.id} sends`,payload)
-    this.port("out").send(payload)
+    this.ports.output.send(payload)
   }
   
   this.receive = function(q)
   {
-    console.log(`${this.id} receives`,q)
     var port = this.port("out")
     for(route_id in port.routes){
       var route = port.routes[route_id];
@@ -149,10 +144,15 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
 
   // REQUEST/ANSWER
 
+  this.answer = function(q)
+  {
+    return this.request(q)
+  }
+
   this.request = function(q)
   {
     var payload = {};
-    var port = this.port("request")
+    var port = this.ports.request
     for(route_id in port.routes){
       var route = port.routes[route_id];
       if(route){
@@ -162,29 +162,7 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
     return payload
   }
 
-  this.answer = function(q)
-  {
-    return this.request(q)
-  }
-
   //
-
-  
-
-  this.install = function(port_id,port_type)
-  {
-    this.ports.push(new Port(this,port_id,port_type))
-  }
-
-
-  this.port = function(target)
-  {
-    for(id in this.ports){
-      var port = this.ports[id];
-      if(port.id == target){ return port; }
-    }
-    console.warn(`Unknown "${target}" port for ${this.id}.`,this,this.ports)
-  }
 
   function Port(host,id,type = PORT_TYPES.default)
   {
@@ -206,12 +184,10 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
     this.connect = function(b,type = "transit")
     {
       this.routes.push(Ø(b))
-      // Ø(b).routes.push({type:type,port:this})
     }
 
     this.send = function(payload) // Send to all routes
     {
-      console.log(this)
       if(this.type != PORT_TYPES.output){ return; }
       for(route_id in this.routes){
         var route = this.routes[route_id];
@@ -232,34 +208,35 @@ function Node(id,rect={x:0,y:0,w:2,h:2},ports=[])
       return null;
     }
   }
-}
 
-function Mesh(id,rect) 
-{
-  Node.call(this,id,rect);
-
-  this.is_mesh = true;
-
-  this.setup = function()
+  function Mesh(id,rect) 
   {
-  }
+    Node.call(this,id,rect);
 
-  this.update = function()
-  {
-    var bounds = {x:0,y:0};
-    for(id in this.children){
-      var node = this.children[id];
-      bounds.x = node.rect.x > bounds.x ? node.rect.x : bounds.x
-      bounds.y = node.rect.y > bounds.y ? node.rect.y : bounds.y
+    this.is_mesh = true;
+
+    this.setup = function()
+    {
     }
-    this.rect.w = bounds.x+4;
-    this.rect.h = bounds.y+5;
+
+    this.update = function()
+    {
+      var bounds = {x:0,y:0};
+      for(id in this.children){
+        var node = this.children[id];
+        bounds.x = node.rect.x > bounds.x ? node.rect.x : bounds.x
+        bounds.y = node.rect.y > bounds.y ? node.rect.y : bounds.y
+      }
+      this.rect.w = bounds.x+4;
+      this.rect.h = bounds.y+5;
+    }
   }
 }
 
 var PORT_TYPES = {default:"default",input:"input",output:"output",request:"request",answer:"answer"}
 var ROUTE_TYPES = {default:"default",request:"request"}
 var NODE_GLYPHS = {
+  default: "M150,60 L150,60 L60,150 L150,240 L240,150 Z",
   router:"M60,60 L60,60 L240,60 M120,120 A30,30 0 0,1 150,150 M150,150 A30,30 0 0,0 180,180 M180,180 L180,180 L240,180 M120,120 L120,120 L60,120 M60,240 L60,240 L240,240 M240,120 L240,120 L180,120 M60,180 L60,180 L120,180",
   entry:"M60,150 L60,150 L240,150 L240,150 L150,240 M150,60 L150,60 L240,150"
 }
